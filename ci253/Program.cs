8d5a3eb2c39c3d8aaa425615bb6600fdbf84f2fd
wf48 <w.fulton1@uni.brighton.ac.uk>
2017-03-06 13:06:18 +0000

    Here is another test commit

--- ci253/Program.cs
+++ ci253/Program.cs
@@ -12,6 +12,7 @@ namespace ci253
         {
             Console.WriteLine("I get back down, and i get down again");
             Console.WriteLine("I get back down, and i get down again");
+            Console.WriteLine("I get down up, and i get up down up down left right left right start select again");
         }
     }
 }
