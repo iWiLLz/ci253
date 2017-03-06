1c0df64d914e3a077c8632ab9f4dffc6143bb36d
jc961 <J.Chatfield1@uni.brighton.ac.uk>
2017-03-06 13:10:08 +0000

    Final Test Commit, in here is descriptive method of how to successfully commit. Enjoy!

--- ci253/Program.cs
+++ ci253/Program.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//How to commit changes in Visual Studios after downloading the project from Git:
+
+//Once you have downloaded the project, open it and connect to your local repo
+//I found that deleting your old LOCAL repo helps Visual Studios find the correct one
+//Don't worry, it will be up on Github so you haven't lost any work
+//Using Team Explorer (View, Team Explorer) select the correct local repo
+//Once you have done this, any changes can be commited by right clicking the project under Solution Explorer, and clicking "Commit"
+//Once you have committed, just sync the projects, and you are ready to go!
+
 namespace ci253
 {
     class Program
